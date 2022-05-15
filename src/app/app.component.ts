5a6decf8d8929c913b8656b583f460ae88150119
Julian <craj.0912@gmail.com>
2022-05-15 14:56:29 -0500

    Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import ICardModel from '../models/card.model'
+import { ICard } from './models/card.model'
 import data from '../data/data.json'
 
 @Component({
@@ -8,18 +8,30 @@ import data from '../data/data.json'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  work!: ICardModel
-  play!: ICardModel
-  study!: ICardModel
-  exercise!: ICardModel
-  social!: ICardModel
-  selfCare!: ICardModel
   time!: string
   selected!: number
+  data!: ICard[]
+  icons: string[] = [
+    'url(./assets/images/icon-work.svg)',
+    'url(./assets/images/icon-play.svg)',
+    'url(./assets/images/icon-study.svg)',
+    'url(./assets/images/icon-exercise.svg)',
+    'url(./assets/images/icon-social.svg)',
+    'url(./assets/images/icon-self-care.svg)'
+  ]
+  colors: string[] = [
+    'hsl(15, 100%, 70%)',
+    'hsl(195, 74%, 62%)',
+    'hsl(348, 100%, 68%)',
+    'hsl(145, 58%, 55%)',
+    'hsl(264, 64%, 52%)',
+    'hsl(43, 84%, 65%)'
+  ]
 
   getData(arr: any[], time: string) {
     return arr.map(elem => (
       {
+        title: elem.title,
         current: elem.timeframes[time].current,
         previous: elem.timeframes[time].previous
       }
@@ -28,42 +40,24 @@ export class AppComponent implements OnInit {
 
   onDaily() {
     this.selected = 1
-    const daily = this.getData(data, 'daily')
+    this.data = this.getData(data, 'daily')
     this.time = 'Day'
-    this.work = daily[0]
-    this.play = daily[1]
-    this.study = daily[2]
-    this.exercise = daily[3]
-    this.social = daily[4]
-    this.selfCare = daily[5]
+
   }
 
   onWeekly() {
     this.selected = 2
-    const weekly = this.getData(data, 'weekly')
+    this.data = this.getData(data, 'weekly')
     this.time = 'Week'
-    this.work = weekly[0]
-    this.play = weekly[1]
-    this.study = weekly[2]
-    this.exercise = weekly[3]
-    this.social = weekly[4]
-    this.selfCare = weekly[5]
   }
 
   onMonthly() {
     this.selected = 3
-    const monthly = this.getData(data, 'monthly')
+    this.data = this.getData(data, 'monthly')
     this.time = 'Month'
-    this.work = monthly[0]
-    this.play = monthly[1]
-    this.study = monthly[2]
-    this.exercise = monthly[3]
-    this.social = monthly[4]
-    this.selfCare = monthly[5]
   }
 
   ngOnInit() {
     this.onWeekly()
   }
-
 }
---
+++ src/app/models/card.model.ts
@@ -0,0 +1,6 @@
+export interface ICard {
+  title: string
+  current: number
+  previous: number
+}
+
